b808f4816656af6dbe878484e22d24233586817f
ayresgit <ayres808@gmail.com>
2022-07-19 17:43:55 -0300

    Change to registers, ISR still not working

--- firmware/src/adc.c
+++ firmware/src/adc.c
@@ -17,7 +17,7 @@ void adc_init(void)
     // configuracao do ADC
     PORTC   =   0b00000000;                         // disables pull-up for adcs pins
     DDRC    =   0b00000000;                         // all adcs as inputs
-    DIDR0   =   0b11111111;                         // ADC0 to ADC2 as adc (digital disable)
+    DIDR0   =   0b11110001;                         // ADC0 to ADC2 as adc (digital disable)
 
     ADMUX   =   (0 << REFS1)                        // AVcc with external capacitor at AREF pin
             | (1 << REFS0)
@@ -33,9 +33,9 @@ void adc_init(void)
             | (1 << ADIE)                           // ADC Interrupt Enable
             | (1 << ADEN)                           // ADC Enable
             | (1 << ADSC)                           // Do the first Start of Conversion
-            | (1 << ADPS2)                          // ADC Prescaller = 128;
-            | (0 << ADPS1)
-            | (1 << ADPS0);
+            | (1 << ADPS2)                          // ADC Prescaller  001 = 2, 010 = 4 ... 111= 128
+            | (1 << ADPS1)                      
+            | (0 << ADPS0);
 
     ADCSRB  =   (1 << ADTS2)                        // Auto-trigger source: timer1 Compare Match B
             | (0 << ADTS1)
@@ -47,7 +47,11 @@ void adc_init(void)
     //TCCR1A  =   (0 << WGM11) | (0 << WGM10)         // mode 2
      //      | (0 << COM1B1) | (0 << COM1B0)         // do nothing
       //     | (0 << COM1A1) | (0 << COM1A0);        // do nothing
-    TCCR1B  = 
+    TCCR1B  =   (0 << WGM13) | (1 << WGM12)
+            | (0 << WGM11) | (0 << WGM10)       // mode ctc
+            | (0 << COM1B1) | (0 << COM1B0)     
+            | (0 << ICNC1) | (0 << ICES1); 
+/*            
 #if ADC_TIMER_PRESCALER ==     1
                 (0 << CS12) | (0 << CS11) | (1 << CS10) // Prescaler N=1
 #elif ADC_TIMER_PRESCALER ==   8
@@ -60,18 +64,14 @@ void adc_init(void)
                 (1 << CS12) | (0 << CS11) | (1 << CS10) // Prescaler N=128
 #else
                 0
-#endif
-                | (0 << WGM13) | (1 << WGM12)
-                | (0 << WGM11) | (0 << WGM10)       // mode ctc
-                | (1 << COM1B1) | (1 << COM1B0)     // Set OC1B on compare match
-                | (0 << ICNC1) | (0 << ICES1);
+#endif*/
 
 
     TCNT1 = 0;              // Disable read/write direct access to the timer counter
     OCR1B = 82;             // OCR1A = TOP = fcpu/(N*2*f) -1
 
-    TIMSK1 |=   (0 << OCIE1A)  | (1 << OCIE1B)        // Ativa a interrupcao na igualdade de comparação do TC1 com OCR1A, desativa OCR1B 
-            | (0 << ICIE1) ;                // Desativa input capture interrupt
+    TIMSK1 |=   (1 << OCIE1B);        // Ativa a interrupcao na igualdade de comparação do TC1 com OCR1A, desativa OCR1B 
+            //| (0 << ICIE1) ;                // Desativa input capture interrupt
 
 #ifdef DEBUG_ON
     set_bit(DDRD, LED1);
@@ -103,7 +103,7 @@ ISR(ADC_vect)
     switch(channel){
         case ADC1:
             batvoltage = adc * batvoltage_coeff;
-            break;
+             break;
 
         case ADC2:                       
             position = adc * position_coeff;
@@ -118,7 +118,9 @@ ISR(ADC_vect)
 #ifdef DEBUG_ON
             set_bit(PORTD, LED1);
 #endif
-            // call control action 
+
+            // 
+
 #ifdef DEBUG_ON
             clr_bit(PORTD, LED1);
 #endif  
@@ -126,7 +128,7 @@ ISR(ADC_vect)
     }
 
     ADMUX = (ADMUX & 0xF1) | ++channel;   // select next channel
-    //ADCSRA = ADCSRA;                  // rearm for next conversion if TIMER0 not in use
+    ADCSRA = ADCSRA;                  // rearm for next conversion if TIMER0 not in use
 
     sei();
 }
