67e76c6784d10f1ff0a3294a72733a8519038edd
ayresgit <ayres808@gmail.com>
2025-06-09 23:04:55 -0300

    different port

--- firmware/up.sh
+++ firmware/up.sh
@@ -4,5 +4,5 @@ make clean
 rm -rf bin obj
 make all
 #fuser -k /dev/ttyACM0
-avrdude -c arduino -P /dev/ttyS8 -p m328p -D -U flash:w:bin/firmware.elf
-stty 57600 < /dev/ttyS8 && cat /dev/ttyS8  
+avrdude -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
+stty 57600 < /dev/ttyACM0 && cat /dev/ttyACM0  
